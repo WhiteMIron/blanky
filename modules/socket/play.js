a126d3a9631f206a513647084d5a8514151757b8
WhiteMIron <bmc131@daum.net>
2021-06-30 17:47:52 +0900

    Answer logic add

--- modules/socket/play.js
+++ modules/socket/play.js
@@ -38,7 +38,252 @@ exports.play = function(socket, io) {
     }
   })
 
+  
+
+ socket.on('answer', async function(data) {
+      answer = data.answer - 1
+      let rightAnswer = socket.blankWords[0]
+      let result = await checkAnswerModule.isRightAnswer(answer, socket.multipleChoiceQuestions, socket.blankWords)
+      let userName = socket.userName
+      let room = socket.room
+      let round =room.round
+      let roundCount = round.count
+      let blankWords = result.blankWords
+      let multipleChoiceQuestions = result.multipleChoiceQuestions
+
+      if (result.isRight == true) {
+        answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
+        answerLength = rightAnswer.length
+        if(Object.keys(socket.wrongAnswerDict).length==0)
+          socket.rightAnswerDict[answerPositionIndex] =answerLength
+        else{
+          for(key of Object.keys(socket.wrongAnswerDict)){
+            if(answerPositionIndex!=key){
+              socket.rightAnswerDict[answerPositionIndex] =answerLength
+            }
+          }
+        }
+       
+          setQuestionStatus(socket,multipleChoiceQuestions,blankWords)
+          messageModule.answerNotify(socket,roundCount,true)
+          messageModule.printMultipleChoiceQuestions(socket,multipleChoiceQuestions)
+          messageModule.broadcastAnswerNotify(socket,userName,roundCount,true)
+        }
+      
+      else if(result.isRight==false){
+        
+        answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
+        answerLength = rightAnswer.length
+     
+        if(Object.keys(socket.wrongAnswerDict)==0)
+          socket.wrongAnswerDict[answerPositionIndex] =answerLength
+        
+        for(key of Object.keys(socket.wrongAnswerDict)){
+          if(key!=answerPositionIndex){
+            socket.wrongAnswerDict[answerPositionIndex] =answerLength
+          }
+        }
+        setQuestionStatus(socket,multipleChoiceQuestions,blankWords)
+        messageModule.answerNotify(socket,roundCount,false)
+        messageModule.printMultipleChoiceQuestions(socket,multipleChoiceQuestions)
+        messageModule.broadcastAnswerNotify(socket,userName,roundCount,false)
+      }
+  
+      result=checkPlayerSolveQuestion(multipleChoiceQuestions)
+      if(result==true){
+          messageModule.roundResultNotify(socket,roundCount)
+      }
+  
+    })
+    
+
+  //>>>>>>>>>>>>>>> [gauge rule apply] answer event  
     // socket.on('answer', async function(data) {
+    //   answer = data.answer - 1
+    //   let rightAnswer = socket.blankWords[0]
+    //   let result = await checkAnswerModule.isRightAnswer(answer, socket.multipleChoiceQuestions, socket.blankWords)
+    //   let userName = socket.userName
+
+    //   let blankWords = result.blankWords
+    //   let multipleChoiceQuestions = result.multipleChoiceQuestions
+
+    //   if (result.isRight == true) {
+     
+    //     answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
+    //     answerLength = rightAnswer.length
+    //     if(Object.keys(socket.wrongAnswerDict).length==0)
+    //       socket.rightAnswerDict[answerPositionIndex] =answerLength
+    //     else{
+    //       for(key of Object.keys(socket.wrongAnswerDict)){
+    //         if(answerPositionIndex!=key){
+    //           socket.rightAnswerDict[answerPositionIndex] =answerLength
+    //         }
+    //       }
+    //     }
+    
+        
+    //     setQuestionStatus(socket,multipleChoiceQuestions,blankWords)
+    //     messageModule.answerNotify(socket,roundCount,true)
+    //     messageModule.printMultipleChoiceQuestions(socket,multipleChoiceQuestions)
+    //     messageModule.broadcastAnswerNotify(socket,userName,roundCount,true)
+    //   }
+    // })
+
+
+    socket.on('recordRoundResult',async function(data){
+       let room = socket.room
+       let players = room.sockets
+       let round = socket.room.round
+       let roundCount = round.count
+       let roundQuestionParagraph = round.questionParagraph
+       let matchHistoryId = socket.matchHistoryId
+       console.log("matchHistoryId",matchHistoryId)
+       // 상대방엔 Lose event emit 
+       messageModule.roundLoseNotify(socket,roundCount)
+    
+       for(player of players){
+        let userName = player.userName
+        let rightAnswerDict = player.rightAnswerDict
+        let wrongAnswerDict = player.wrongAnswerDict
+        let blankWords =player.blankWords
+        for( word of blankWords){
+          let rightAnswer = word
+          wrongAnswerDict = player.wrongAnswerDict
+          answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
+          answerLength = rightAnswer.length
+      
+          if(Object.keys(player.wrongAnswerDict)==0)
+            player.wrongAnswerDict[answerPositionIndex] =answerLength
+          
+          for(key of Object.keys(player.wrongAnswerDict)){
+            if(key!=answerPositionIndex){
+              player.wrongAnswerDict[answerPositionIndex] =answerLength
+            }
+          }
+        }
+   
+        rightAnswerDict = player.rightAnswerDict
+        wrongAnswerDict = player.wrongAnswerDict
+        
+        console.log(rightAnswerDict)
+        console.log(wrongAnswerDict)
+        roundHistoryId=await matchService.recordRoundHistory(roundCount,roundQuestionParagraph,true,matchHistoryId,userName)         
+        console.log("roundHistoryId:",roundHistoryId)
+        for(answerPositionIndex of Object.keys(rightAnswerDict))
+          await matchService.recordAnswerHistory(answerPositionIndex,rightAnswerDict[answerPositionIndex],true,roundHistoryId)
+        for(answerPositionIndex of Object.keys(wrongAnswerDict))
+          await matchService.recordAnswerHistory(answerPositionIndex,wrongAnswerDict[answerPositionIndex],false,roundHistoryId)
+ 
+      }
+      
+       //여기다 라운드 체크하고 끝낼 건지 문제를 더 출제할건지에 대한 코드 작성해야함 
+       if(roundCount<2){
+        plusRound(room)
+        let questionMsg = await createQuestion()
+        console.log("questionMsg:",questionMsg)
+        sendQuestion(io, room, questionMsg)
+        let players = room.sockets
+        setPlayersQuestionStatus(players, questionMsg.multipleChoiceQuestions, questionMsg.blankWords)
+        round.questionParagraph = questionMsg.originalParagraph
+
+        }
+        else{
+          io.to(room.name).emit("finish")
+          leaveRoom(room)
+        }
+      })
+
+}
+
+
+function checkAllPlayerSolveQuestion(players){
+  for(player of players){
+   result =checkPlayerSolveQuestion(player.multipleChoiceQuestions)
+   if(result==true)
+    continue
+   else
+    return false
+  }
+  return true
+}
+
+function checkPlayerSolveQuestion(multipleChoiceQuestions){
+  if(multipleChoiceQuestions.length ==0)
+    return true
+  else 
+    return false
+}
+
+function plusRound(room){
+  room.round.count ++
+}
+
+
+function setQuestionStatus(player,multipleChoiceQuestions,blankWords){
+  player.multipleChoiceQuestions = multipleChoiceQuestions
+  player.blankWords = blankWords
+}
+
+
+function checkReadyPlayer(room,socket){
+  if(room.readyPlayers.indexOf(socket) ==-1)
+    room.readyPlayers.push(socket)
+}
+
+
+function isReadyRoom(room, socket) {
+    if (room.readyPlayers.length == 2){
+      room.status = constants.ready
+      return constants.ready
+    }
+  else
+    return constants.wait
+}
+
+
+async function createQuestion(){
+    let question = await questionModule.createQuestion()
+    let questionMsg = {
+           originalParagraph: question.originalParagraph,
+           question: question.paragraph,
+           multipleChoiceQuestions : question.multipleChoiceQuestions,
+           blankWords:question.blankWords
+         }
+   return questionMsg
+}
+
+function sendQuestion(io,room,questionMsg){
+  let roomName = room.name
+  messageModule.broadcastQuestion(io,roomName,questionMsg)
+}
+
+function setPlayersQuestionStatus (players,multipleChoiceQuestions,blankWords){
+  for( player of players)
+    setQuestionStatus(player,multipleChoiceQuestions,blankWords)
+}
+
+function setQuestionStatus(player,multipleChoiceQuestions,blankWords){
+  player.multipleChoiceQuestions = multipleChoiceQuestions
+  player.blankWords = blankWords
+}
+
+function setPlayStatusRoom(room){
+  room.readyPlayers.splice(0,room.readyPlayers.length)
+  room.status = constants.play
+}
+
+function leaveRoom(room){
+  let players = room.sockets
+  for( player of players){
+    player.leave(room.name)
+    player.room = null
+    player.emit("leaveRoom")
+  }
+
+}
+
+
+ // socket.on('answer', async function(data) {
     //   answer = data.answer - 1
     //   let rightAnswer = socket.blankWords[0]
     //   let result = await checkAnswerModule.isRightAnswer(answer, socket.multipleChoiceQuestions, socket.blankWords)
@@ -147,226 +392,3 @@ exports.play = function(socket, io) {
      
     //   }
     // })
-
-
- socket.on('answer', async function(data) {
-      answer = data.answer - 1
-      let rightAnswer = socket.blankWords[0]
-      let result = await checkAnswerModule.isRightAnswer(answer, socket.multipleChoiceQuestions, socket.blankWords)
-      let userName = socket.userName
-      let room = socket.room
-      let round =room.round
-      let roundCount = round.count
-      console.log("roundCount:",roundCount)
-      let blankWords = result.blankWords
-      let multipleChoiceQuestions = result.multipleChoiceQuestions
-
-      if (result.isRight == true) {
-     
-        answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
-        answerLength = rightAnswer.length
-        if(Object.keys(socket.wrongAnswerDict).length==0)
-          socket.rightAnswerDict[answerPositionIndex] =answerLength
-        else{
-          for(key of Object.keys(socket.wrongAnswerDict)){
-            if(answerPositionIndex!=key){
-              socket.rightAnswerDict[answerPositionIndex] =answerLength
-            }
-          }
-        }
-        result=checkPlayerSolveQuestion(multipleChoiceQuestions)
-        if(result==true){
-          
-            messageModule.roundResultNotify(socket,roundCount)
-        }
-    
-        else{    
-          setQuestionStatus(socket,multipleChoiceQuestions,blankWords)
-          messageModule.answerNotify(socket,roundCount,true)
-          messageModule.printMultipleChoiceQuestions(socket,multipleChoiceQuestions)
-          messageModule.broadcastAnswerNotify(socket,userName,roundCount,true)
-        }
-      }
-    })
-    
-
-  //>>>>>>>>>>>>>>> [gauge rule apply] answer event  
-    // socket.on('answer', async function(data) {
-    //   answer = data.answer - 1
-    //   let rightAnswer = socket.blankWords[0]
-    //   let result = await checkAnswerModule.isRightAnswer(answer, socket.multipleChoiceQuestions, socket.blankWords)
-    //   let userName = socket.userName
-
-    //   let blankWords = result.blankWords
-    //   let multipleChoiceQuestions = result.multipleChoiceQuestions
-
-    //   if (result.isRight == true) {
-     
-    //     answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
-    //     answerLength = rightAnswer.length
-    //     if(Object.keys(socket.wrongAnswerDict).length==0)
-    //       socket.rightAnswerDict[answerPositionIndex] =answerLength
-    //     else{
-    //       for(key of Object.keys(socket.wrongAnswerDict)){
-    //         if(answerPositionIndex!=key){
-    //           socket.rightAnswerDict[answerPositionIndex] =answerLength
-    //         }
-    //       }
-    //     }
-    
-        
-    //     setQuestionStatus(socket,multipleChoiceQuestions,blankWords)
-    //     messageModule.answerNotify(socket,roundCount,true)
-    //     messageModule.printMultipleChoiceQuestions(socket,multipleChoiceQuestions)
-    //     messageModule.broadcastAnswerNotify(socket,userName,roundCount,true)
-    //   }
-    // })
-
-
-    socket.on('recordRoundResult',async function(data){
-       let room = socket.room
-       let players = room.sockets
-       let round = socket.room.round
-       let roundCount = round.count
-       let roundQuestionParagraph = round.questionParagraph
-       let matchHistoryId = socket.matchHistoryId
-       console.log("matchHistoryId",matchHistoryId)
-       // 상대방엔 Lose event emit 
-       messageModule.roundLoseNotify(socket,roundCount)
-    
-       for(player of players){
-        let userName = player.userName
-        let rightAnswerDict = player.rightAnswerDict
-        let wrongAnswerDict = player.wrongAnswerDict
-        let blankWords =player.blankWords
-        for( word of blankWords){
-          let rightAnswer = word
-          wrongAnswerDict = player.wrongAnswerDict
-          answerPositionIndex=round.questionParagraph.indexOf(rightAnswer)
-          answerLength = rightAnswer.length
-      
-          if(Object.keys(player.wrongAnswerDict)==0)
-            player.wrongAnswerDict[answerPositionIndex] =answerLength
-          
-          for(key of Object.keys(player.wrongAnswerDict)){
-            if(key!=answerPositionIndex){
-              player.wrongAnswerDict[answerPositionIndex] =answerLength
-            }
-          }
-        }
-   
-        rightAnswerDict = player.rightAnswerDict
-        wrongAnswerDict = player.wrongAnswerDict
-        
-        console.log(rightAnswerDict)
-        console.log(wrongAnswerDict)
-        roundHistoryId=await matchService.recordRoundHistory(roundCount,roundQuestionParagraph,true,matchHistoryId,userName)         
-        console.log("roundHistoryId:",roundHistoryId)
-        for(answerPositionIndex of Object.keys(rightAnswerDict))
-          await matchService.recordAnswerHistory(answerPositionIndex,rightAnswerDict[answerPositionIndex],true,roundHistoryId)
-        for(answerPositionIndex of Object.keys(wrongAnswerDict))
-          await matchService.recordAnswerHistory(answerPositionIndex,wrongAnswerDict[answerPositionIndex],false,roundHistoryId)
- 
-      }
-      
-       //여기다 라운드 체크하고 끝낼 건지 문제를 더 출제할건지에 대한 코드 작성해야함 
-       if(roundCount<2){
-        plusRound(room)
-        let questionMsg = await createQuestion()
-        console.log("questionMsg:",questionMsg)
-        sendQuestion(io, room, questionMsg)
-        let players = room.sockets
-        setPlayersQuestionStatus(players, questionMsg.multipleChoiceQuestions, questionMsg.blankWords)
-        round.questionParagraph = questionMsg.originalParagraph
-
-        }
-      })
-
-}
-
-
-function checkAllPlayerSolveQuestion(players){
-  for(player of players){
-   result =checkPlayerSolveQuestion(player.multipleChoiceQuestions)
-   if(result==true)
-    continue
-   else
-    return false
-  }
-  return true
-}
-
-function checkPlayerSolveQuestion(multipleChoiceQuestions){
-  if(multipleChoiceQuestions.length ==0)
-    return true
-  else 
-    return false
-}
-
-function plusRound(room){
-  room.round.count ++
-}
-
-
-function setQuestionStatus(player,multipleChoiceQuestions,blankWords){
-  player.multipleChoiceQuestions = multipleChoiceQuestions
-  player.blankWords = blankWords
-}
-
-
-function checkReadyPlayer(room,socket){
-  if(room.readyPlayers.indexOf(socket) ==-1)
-    room.readyPlayers.push(socket)
-}
-
-
-function isReadyRoom(room, socket) {
-    if (room.readyPlayers.length == 2){
-      room.status = constants.ready
-      return constants.ready
-    }
-  else
-    return constants.wait
-}
-
-
-async function createQuestion(){
-    let question = await questionModule.createQuestion()
-    let questionMsg = {
-           originalParagraph: question.originalParagraph,
-           question: question.paragraph,
-           multipleChoiceQuestions : question.multipleChoiceQuestions,
-           blankWords:question.blankWords
-         }
-   return questionMsg
-}
-
-function sendQuestion(io,room,questionMsg){
-  let roomName = room.name
-  messageModule.broadcastQuestion(io,roomName,questionMsg)
-}
-
-function setPlayersQuestionStatus (players,multipleChoiceQuestions,blankWords){
-  for( player of players)
-    setQuestionStatus(player,multipleChoiceQuestions,blankWords)
-}
-
-function setQuestionStatus(player,multipleChoiceQuestions,blankWords){
-  player.multipleChoiceQuestions = multipleChoiceQuestions
-  player.blankWords = blankWords
-}
-
-function setPlayStatusRoom(room){
-  room.readyPlayers.splice(0,room.readyPlayers.length)
-  room.status = constants.play
-}
-
-function leaveRoom(room){
-  let players = room.sockets
-  for( player of players){
-    player.leave(room.name)
-    player.room = null
-    player.emit("leaveRoom")
-  }
-
-}
